134cf8bb1549435b05de7e969dcd47a1f9b91b8b
ga731852 <ga731852@gmail.com>
2022-03-16 01:08:21 +0800

    add swag function

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module tsmc.com/go-gin-docker
 go 1.16
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/gin-swagger v1.4.1
+	github.com/swaggo/swag v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
+	golang.org/x/tools v0.1.9 // indirect
 )
--- main.go
+++ main.go
@@ -1,45 +1,50 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
-	"time"
 
-	"github.com/EDDYCJY/go-gin-example/routers"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"tsmc.com/go-gin-docker/repository"
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	"tsmc.com/go-gin-docker/Customer/repository"
+	"tsmc.com/go-gin-docker/Customer/routers"
+	_ "tsmc.com/go-gin-docker/docs"
 )
 
 func DBConnection() (db *sql.DB) {
 	// service_url := "oracle://GA731852:uwygnnr2@192.168.1.114:1521/ORCLCDB.localdomain"
-	db, err := sql.Open("mysql", "root:usbw@tcp(192.168.1.105:3306)/classicmodels")
+	db, err := sql.Open("mysql", "root:test@tcp(localhost:3306)/classicmodels")
 	if err != nil {
 		fmt.Println("1err", err)
 	}
 	return db
 }
 
+// @title Gin swagger
+// @version 1.0
+// @description Gin swagger
+
+// @contact.name tlchoud
+// @contact.url test
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// schemes http
 func main() {
 
 	mariaDB := DBConnection()
 	defer mariaDB.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-	defer cancel()
 
 	mariaRepo := repository.NewMariaRepository(mariaDB)
-	res, err := mariaRepo.GetCustomers(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
 
-	for _, v := range res {
-		fmt.Println(v)
-	}
-	// port := os.Getenv("PORT")
-	routersInit := routers.InitRouter()
-	readTimeout := 60 * time.Second
-	writeTimeout := 60 * time.Second
-	endPoint := "8000"
-	maxHeaderBytes := 1 << 20
+	r := gin.Default()
+	// gin.SetMode(gin.ReleaseMode)
+	routers.NewMariaRouter(r, mariaRepo)
+	//TODO: add mariadb resource to mac
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.Run(":8080")
 }
